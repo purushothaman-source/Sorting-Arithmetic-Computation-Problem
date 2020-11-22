7178f14045588671da73c62ffdeb3107e9b90d5d
purushothaman-source <purushoth99jun@gmail.com>
2020-11-22 21:06:46 +0530

    ADDED:stored the results in Dictonarty

--- sort.sh
+++ sort.sh
@@ -10,3 +10,5 @@ result3=$(echo $num1 $num2 $num3 |awk '{ print $3+$1/$2}')
 echo "Result3:$result3"
 result4=$((num1%num2+num3))
 echo "Result4:$result4"
+declare -A  values=( [0]=$result1 [1]=$result2 [2]=$result3 [3]=$result4 )
+echo "Stored numbers:${values[@]}"
